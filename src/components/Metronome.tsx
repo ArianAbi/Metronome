e67f6a5ed1397f3c096cbbfde151d48cff5cbcc2
arian <kapqworks@gmail.com>
2023-09-12 01:19:21 +0330

    init , simple metronome logic

---
+++ src/components/Metronome.tsx
@@ -0,0 +1,67 @@
+import '../../global.css'
+import { useState, useEffect } from 'react'
+
+export default function Metronome() {
+
+    const [metronome, setMetronome] = useState<null | number>(null);
+    const [tempo, setTempo] = useState(100);
+    const [tick, setTick] = useState(false);
+
+    const secondInMs = 60000;
+    const tickDuration = secondInMs / tempo;
+
+    //on tempo change update the interval
+    useEffect(() => {
+        if (metronome) {
+            clearInterval(metronome)
+            setMetronome(setInterval(metronomeTick, tickDuration))
+        }
+    }, [tempo])
+
+    function toggleMetronome() {
+        //start if we dont have a metronome
+        if (!metronome) {
+            setMetronome(setInterval(metronomeTick, tickDuration))
+
+            return
+        }
+
+        //else stop the metronome
+        clearInterval(metronome)
+        setMetronome(null)
+    }
+
+
+    function metronomeTick() {
+        setTick(true)
+        setTimeout(() => {
+            setTick(false)
+        }, tickDuration / 4);
+    }
+
+    return (
+        <>
+            <div className="flex flex-col justify-center items-center gap-2">
+                <h2 className="font-bold">Metronome</h2>
+                <div className="flex flex-col items-center justify-center">
+                    <label title="tempo">Tempo</label>
+                    <input
+                        id="tempo"
+                        type="number"
+                        value={tempo}
+                        onChange={(e: any) => setTempo(e.target.value)}
+                        className="border-2 border-gray-700 px-2"
+                    />
+                </div>
+                <button
+                    className={`bg-green-700 py-2 px-4 rounded-lg text-white font-bold`}
+                    onClick={() => toggleMetronome()}
+                >
+                    {!metronome ? 'Start Metronome' : 'Stop Metronome'}
+                </button>
+
+                <div className={`${tick ? 'bg-red-500' : 'bg-gray-500'} w-24 h-24`}></div>
+            </div>
+        </>
+    )
+}
